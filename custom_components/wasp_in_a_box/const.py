0626ae0962ef44c7e749fce41c5f2adf3a892392
Andrew Jackson <andrew@codechimp.org>
2026-02-25 08:26:49 +0000

    Remove unused constants

--- custom_components/wasp_in_a_box/const.py
+++ custom_components/wasp_in_a_box/const.py
@@ -1,8 +1,6 @@
 """Constants for wasp_in_a_box."""
 
-import json
 from logging import Logger, getLogger
-from pathlib import Path
 
 from homeassistant.const import Platform
 
@@ -10,15 +8,7 @@ LOGGER: Logger = getLogger(__package__)
 
 MIN_HA_VERSION = "2026.1"
 
-manifestfile = Path(__file__).parent / "manifest.json"
-with manifestfile.open(encoding="UTF-8") as json_file:
-    manifest_data = json.load(json_file)
-
-DOMAIN = manifest_data.get("domain")
-NAME = manifest_data.get("name")
-VERSION = manifest_data.get("version")
-ISSUEURL = manifest_data.get("issue_tracker")
-CONFIG_VERSION = 1
+DOMAIN = "wasp_in_a_box"
 
 PLATFORMS = [Platform.BINARY_SENSOR]
 
